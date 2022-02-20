0f7f381402a8fb18746581a2974a5c183b888f0b
Ran Milshtein <ranmilshtein@gmail.com>
2022-02-20 17:40:15 +0200

    changed resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources: parking_spot do
-    resources: booking only: [:new, :create, :show, :update]
+  resources :parking_spot do
+    resources :booking, only: [:new, :create, :show]
   end
+  resources :booking, only: [:delete, :update]
 end
