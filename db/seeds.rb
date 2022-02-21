23b2aa23785627859a6f58610e601de92f1283ee
Laura Sucher <l.sucher@gmx.de>
2022-02-21 11:57:50 +0200

    Implemented the index and show views for the parkingcontroller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+ParkingSpot.destroy_all
+
+puts "Creating movies..."
+parking1 = { title: "Wonderful parkingspot", user_id: 1 }
+parking2 = { title: "Fantastic parkingspot", user_id: 1 }
+parking3 = { title: "Perfect parkingspot", user_id: 1 }
+parking4 = { title: "Amazing parkingspot", user_id: 1 }
+
+[parking1, parking2, parking3, parking4].each do |attributes|
+  parking = ParkingSpot.create!(attributes)
+  puts "Created #{parking.title}"
+end
