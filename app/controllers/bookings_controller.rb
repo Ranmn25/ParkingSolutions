6ea3ca0c2659c2f8d0d28a8b03daacd997210085
Ran Milshtein <ranmilshtein@gmail.com>
2022-02-21 12:08:10 +0200

    did some work on controller and views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,21 @@
 class BookingsController < ApplicationController
-  def index
-    @booking = Booking.all
+  def show
+    @booking = Booking.find(params[])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :time, :status, :total_price)
   end
 end
