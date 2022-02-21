b669ec60a26d538b28b74ca5263270a2f8dc49cd
Laura Sucher <l.sucher@gmx.de>
2022-02-21 12:45:36 +0200

    Added new and create method and views for parking

--- app/controllers/parking_spots_controller.rb
+++ app/controllers/parking_spots_controller.rb
@@ -6,4 +6,24 @@ class ParkingSpotsController < ApplicationController
   def show
     @parking_spot = ParkingSpot.find(params[:id])
   end
+
+  def new
+    @parking_spot = ParkingSpot.new
+  end
+
+  def create
+    @parking_spot = ParkingSpot.new(parking_params)
+    @parking_spot.user = current_user
+    if @parking_spot.save
+      redirect_to @parking_spot, notice: 'Parking spot was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def parking_params
+    params.require(:parking_spot).permit(:title, :address, :descrition, :price_per_hour)
+  end
 end
