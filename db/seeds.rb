a14ceaaa36a77141524dfb3ba26f150e8bd29291
Talia Azulay <modeoasis1@gmail.com>
2022-02-23 13:07:47 +0200

    change images

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,11 +23,10 @@ parking_spot2 = ParkingSpot.create!(parking2)
 parking_spot3 = ParkingSpot.create!(parking3)
 parking_spot4 = ParkingSpot.create!(parking4)
 
-file1 = URI.open('https://images.unsplash.com/photo-1583073491690-3adcab1b7bdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=')
-file2 = URI.open('https://images.unsplash.com/photo-1583073491690-3adcab1b7bdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=')
-file3 = URI.open('https://images.unsplash.com/photo-1583073491690-3adcab1b7bdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=')
+file1 = URI.open('https://images.unsplash.com/photo-1522318548694-a0b65c40b0c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjV8fHBhcmtpbmclMjBzcGFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60')
+file2 = URI.open('https://images.unsplash.com/photo-1577059023365-cb3d8a8e1e9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fHBhcmtpbmclMjBzcGFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60')
+file3 = URI.open('https://images.unsplash.com/photo-1534791108293-e080c7d856e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fHBhcmtpbmclMjBzcGFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60')
 file4 = URI.open('https://images.unsplash.com/photo-1583073491690-3adcab1b7bdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=')
-
 
 parking_spot1.photo.attach(io: file1, filename: 'image1.png', content_type: 'image/png')
 parking_spot2.photo.attach(io: file2, filename: 'image2.png', content_type: 'image/png')
